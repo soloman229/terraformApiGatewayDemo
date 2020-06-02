2f0ab3a6c4f31103b81f2594aa035767bdcbd9d7
rahul kinge <rahulkinge22@gmail.com>
2020-06-02 20:46:32 +0530

    add variable file

--- apigateway/provider.tf
+++ apigateway/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
-  access_key = AKIA3JFV3RLYDE3GSZHM
-  secret_key = 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
+  region = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
---
+++ apigateway/variable.tf
@@ -0,0 +1,15 @@
+variable "aws_access_key" {
+  type = string
+  description = "AWS access key"
+  default = "AKIA3JFV3RLYPX33T5WE"
+}
+variable "aws_secret_key" {
+  type = string
+  description = "AWS secret key"
+  default = "PBMu+DIPz0TNQbhd9d9TFIhQqn/GgPMeB62tspml"
+}
+variable "aws_region" {
+  type = string
+  description = "AWS region"
+  default = "us-east-1"
+}
